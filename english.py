35db688453bcd068dd6c4cc48d4dd19c92d4d339
Ning An <1196244890@qq.com>
2022-06-02 15:40:23 +0200

    misspell

--- english.py
+++ english.py
@@ -4,19 +4,20 @@ import csv
 
 def addTrans():
     new_row = []
-    with open('C:/Users/HP/Desktop/english2.csv') as f:
+    with open('C:/Users/HP/Desktop/english11.csv') as f:
         for row in f:
             url = f'http://dict.youdao.com/search?q={row}'
             print(url)
             content = urllib.request.urlopen(url)
             pattern = re.compile("</h2.*?</ul>", re.DOTALL)
+            # TODO: when the spell is wrong, give some prompt.
             result = pattern.search(content.read().decode()).group()
             pattern2 = re.compile('<li>.*?</li>')
             trans = [row]
             for meta in pattern2.findall(result):
                 trans.append(meta.strip('<li>').strip('<li>'))
             new_row.append(trans)
-        with open('C:/Users/HP/Desktop/english2.csv', 'w', newline='') as ff:
+        with open('C:/Users/HP/Desktop/english11.csv', 'w', newline='') as ff:
             writer = csv.writer(ff)
             i = 0
             for nrow in new_row:
