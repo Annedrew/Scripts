f402b6220dc122901fe55a1840af222dcdb5ffeb
Ning An <1196244890@qq.com>
2022-06-02 12:24:27 +0200

    add_translation

---
+++ english.py
@@ -0,0 +1,25 @@
+import urllib.request
+import re
+import csv
+
+def addTrans():
+    new_row = []
+    with open('C:/Users/HP/Desktop/english2.csv') as f:
+        for row in f:
+            url = f'http://dict.youdao.com/search?q={row}'
+            print(url)
+            content = urllib.request.urlopen(url)
+            pattern = re.compile("</h2.*?</ul>", re.DOTALL)
+            result = pattern.search(content.read().decode()).group()
+            pattern2 = re.compile('<li>.*?</li>')
+            trans = [row]
+            for meta in pattern2.findall(result):
+                trans.append(meta.strip('<li>').strip('<li>'))
+            new_row.append(trans)
+        with open('C:/Users/HP/Desktop/english2.csv', 'w', newline='') as ff:
+            writer = csv.writer(ff)
+            i = 0
+            for nrow in new_row:
+                writer.writerow(nrow)
+                i += 1
+                print(f'{i} row has added.')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import english
 
 if __name__ == '__main__':
-    None
+    english.addTrans()
