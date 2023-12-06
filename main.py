316cf9270ddba9e64dab47156a75e9ff08023b97
Annedrew <1196244890@qq.com>
2023-12-06 12:36:39 +0100

    [notion] extract options from questions.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import english
-import exchange
+# import english
+# import exchange
+import notion
 
 
 if __name__ == '__main__':
     # english.addTrans()
-    exchange.get_exchange()
+    # exchange.get_exchange()
+
+    # notion:
+    pdf_path = "AWS Certified Cloud Practitioner CLF-C02.pdf"
+    total_question_number = 2
+    for question_number in range(1, (total_question_number + 1)):
+        options = notion.extract_options_from_pdf(pdf_path, question_number)
+        notion.write_txt(options, question_number)
+        print(f"{question_number} is written.")
