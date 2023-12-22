c6f5e5f9ce82ccd8caac29512bc605cbd8e1e681
Annedrew <1196244890@qq.com>
2023-12-22 15:54:31 +0100

    minor change

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ if __name__ == '__main__':
     # exchange.get_exchange()
 
     # notion:
-    pdf_path = "AWS Certified Cloud Practitioner CLF-C02.pdf"
-    total_question_number = 2
-    for question_number in range(1, (total_question_number + 1)):
+    pdf_path = "AWS Certified Cloud Practitioner CLF-C02(1-5pages).pdf"
+    start_question_number = 624
+    end_question_number = 669
+    for question_number in range(start_question_number, (end_question_number + 1)):
         options = notion.extract_options_from_pdf(pdf_path, question_number)
         notion.write_txt(options, question_number)
         print(f"{question_number} is written.")
