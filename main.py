312c31dd1971a617aaef2867f8076b75573ae8aa
Annedrew <1196244890@qq.com>
2023-12-28 00:41:47 +0100

    update

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # import english
 # import exchange
-import pdf_extract_aws
+# import pdf_extract_aws
+import movie_scraping
 
 
 if __name__ == '__main__':
     # english.addTrans()
     # exchange.get_exchange()
 
-    # notion:
-    pdf_path = "AWS Certified Cloud Practitioner CLF-C02(1-5pages).pdf"
-    start_question_number = 624
-    end_question_number = 669
-    for question_number in range(start_question_number, (end_question_number + 1)):
-        options = pdf_extract_aws.extract_options_from_pdf(pdf_path, question_number)
-        pdf_extract_aws.write_txt(options, question_number)
-        print(f"{question_number} is written.")
+    # pdf_extract_aws:
+    # pdf_path = "AWS Certified Cloud Practitioner CLF-C02(1-5pages).pdf"
+    # start_question_number = 624
+    # end_question_number = 669
+    # for question_number in range(start_question_number, (end_question_number + 1)):
+    #     options = pdf_extract_aws.extract_options_from_pdf(pdf_path, question_number)
+    #     pdf_extract_aws.write_txt(options, question_number)
+    #     print(f"{question_number} is written.")
+
+    # movie_scraping
+    web_url = "https://www.rottentomatoes.com/"
+    movie_scraping.access_page(web_url)
--- movie_scraping.py
+++ movie_scraping.py
@@ -1,1 +1,24 @@
 # Scatping the top box office movie from rotten tomatoes
+
+from selenium import webdriver
+
+def access_page(web_url):
+    # 1. Open "Rotten Tomatoes" website
+    browser = webdriver.Chrome()
+    browser.get(web_url)
+
+    browser.close()
+
+# 2. Find the "movie" tab
+# 3. Hover(not press) to the "movie" button 
+# 4. Find "Top box office" button
+# 5. Press "Top box office" button
+# 6. Find "TOMATOMETER" tab
+# 7. Press "TOMATOMETER" button
+# 8. Find the first button "CERTIFIED FRESH"
+# 9. Choose "CERTIFIED FRESH"
+# 10. Find "APPLY" button
+# 11. Press "APPLY" button
+# 12. Get the first movie element
+# 13. Get the name of the movie
+# 14. Save the name into Database
